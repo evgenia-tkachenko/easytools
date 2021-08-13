638f54d0a2eaeb20c63b8747647ad0367bcd9a28
Evgenia Tkachenko <evgeniatkachenko24@gmail.com>
2021-08-13 10:39:53 +0300

    added functions to check package

---
+++ functions.py
@@ -0,0 +1,6 @@
+def hello():
+    print('Hello from functions.py')
+
+def print_numpy_version():
+    import numpy as np
+    print(np.__version__)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='easytools',
-    version='0.0.0',
+    version='0.0.1',
     author='Evgenia Tkachenko',
     author_email='evgeniatkachenko24@gmail.com',
     description='Testing installation of Package',
