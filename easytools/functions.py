ccbdbd6469e0359b2742d2f9c92f53b98ab73493
Evgenia Tkachenko <evgeniatkachenko24@gmail.com>
2021-08-13 14:56:07 +0300

    modified func output, requirements, gitignore

--- easytools/functions.py
+++ easytools/functions.py
@@ -1,6 +1,6 @@
 def print_numpy_version():
     import numpy as np
-    print(np.__version__)
+    print('Numpy version in your env is ', np.__version__)
 
 def hello():
     print('Hello from functions.py')
